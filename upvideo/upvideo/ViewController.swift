4ad07ff7c17468d9c031e262867bf252b096d5e4
mac126 <tulaomao152@126.com>
2018-05-16 16:35:49 +0800

    上传视频到服务器，基于AFNetworking

--- upvideo/upvideo/ViewController.swift
+++ upvideo/upvideo/ViewController.swift
@@ -11,6 +11,7 @@ import AFNetworking
 
 class ViewController: UIViewController {
 
+    let uploadUrl = "http://192.168.1.149/upvideo/upload.php"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +20,44 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func uploadButtonClick(_ sender: UIButton) {
+        uploadVideo()
+        
+    }
+    
+    //上传视频到服务器
+    private func uploadVideo(){
+        let urlString = "http://192.168.1.149/upvideo/upload.php"
+        let request = AFHTTPRequestSerializer().multipartFormRequest(withMethod: "POST", urlString: urlString, parameters: nil, constructingBodyWith: { (formdata) in
+            let fileURL = Bundle.main.url(forResource: "hh", withExtension: "MP4")!
+            do {
+                try formdata.appendPart(withFileURL: fileURL, name: "file", fileName: "yy.mp4", mimeType: "video/mp4")
+            } catch {
+                print("formdataError")
+            }
+        }, error: nil)
+        
+        let manager = AFHTTPSessionManager(sessionConfiguration: URLSessionConfiguration.default)
+        let uploadTask = manager.uploadTask(withStreamedRequest: request as URLRequest, progress: { (uploadProgress) in
+            
+        }) { (response, responseObject, error) in
+            if let error = error {
+                print("-error-\(error)")
+            } else {
+                print("-response-\(response)-responseObject-\(responseObject)")
+            }
+        }
+        
+        uploadTask.resume()
+    }
+    
+    private func alert(withMessage message: String) {
+        let alert = UIAlertController(title:"提示",message:message, preferredStyle: .alert)
+        let action2 = UIAlertAction(title: "关闭", style: .default, handler: nil)
+        alert.addAction(action2)
+        self.present(alert , animated: true , completion: nil)
     }
 
 
